431f591f6f9f7bd1b7999b60fbcdacf17febe6ad
only-polifill <740269390@qq.com>
2021-06-28 23:37:42 +0800

    projectinfo验证

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const inquirer = require('inquirer')
+const semver = require('semver')
 
 async function init () {
     return await getProjectInfo()
@@ -18,7 +19,21 @@ async function getProjectName () {
     return await inquirer.prompt({
         type: 'input',
         name: 'projectName',
-        message: '请输入项目名称'
+        message: '请输入项目名称',
+        default: '',
+        validate: function (v) {
+            const done = this.async()
+            setTimeout(() => {
+                if (!/^[a-zA-Z]+[-][a-zA-Z0-9]*|[_][a-zA-Z0-9]*$/.test(v)) {
+                    done('请输入正确的项目名称')
+                    return
+                }
+                done(null, true)
+            }, 0)
+        },
+        filter: function (v) {
+            return v
+        }
     })
 }
 
@@ -26,7 +41,25 @@ async function getProjectVersion () {
     return await inquirer.prompt({
         type: 'input',
         name: 'projectVersion',
-        message: '请输入项目版本号'
+        message: '请输入项目版本号',
+        default: '1.0.0',
+        validate: function (v) {
+            const done = this.async()
+            setTimeout(() => {
+                if (!(!!semver.valid(v))) {
+                    done('请输入正确的版本号 如：1.0.0 v1.0.0')
+                    return
+                }
+                done(null, true)
+            }, 0)
+        },
+        filter: function (v) {
+            if (!!semver.valid(v)) {
+                return semver.valid(v);
+            } else {
+                return v;
+            }
+        }
     })
 }
 
